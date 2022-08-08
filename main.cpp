e2ce1f068b7c22501efd139149a367cc9d23154c
Bovera <1847845057@qq.com>
2022-08-08 17:36:45 +0800

    mpi

--- main.cpp
+++ main.cpp
@@ -1,4 +1,9 @@
 #include <fstream>
+#include <cstdio>
+
+#ifdef USE_MPI
+#include <mpi.h>
+#endif
 
 #include "molecule.h"
 #include "model.h"
@@ -21,13 +26,13 @@
     double range = 1.0;     // 打乱的范围（退火时粒子随机移动的范围）
 ************************************************************/
 
-int main() {
-    
+void thread(int glide, int n) {
     // 默认模型参数 default_para
     para ppara = default_para;
     // 修改模型参数，比如 m,n
-    ppara.m = 12;
-    ppara.repeat = 10;
+    ppara.n = n;
+    ppara.repeat = 100;
+    ppara.glide = glide;
     // 以 ppara 参数生成模型
     // 模型已经生成，之后再修改模型参数也没用
     model this_model(ppara);
@@ -40,11 +45,13 @@ int main() {
     this_model.disorganize();
     
     // dump 文件
-    std::ofstream fout("test.dump");
+    char name[20];
+    std::sprintf(name, "test_%d_%d.dump", glide, n);
+    std::ofstream fout(name);
     
     // 声明图片类
     // figure energy_change;
-    for (int k=0; k<1000; k++) {
+    for (int k=0; k<100; k++) {
         // 模型计算更新
         this_model.update();
         if (k%10==0) {
@@ -59,6 +66,38 @@ int main() {
     
     // 保存 dump 文件
     fout.close();
+}
+
+int main(int argc, char* argv[]) {
+
+    #ifdef USE_MPI
+    int size;
+    int rank;
+    MPI_Init(&argc, &argv);
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    int task = 35 / size + 1;
+    int count = 0;
+    #endif
+
+    for (int n=7; n<14; n++)
+    for (int glide=0; glide<5; glide++) {
+
+    #ifdef USE_MPI
+    if (count % task == rank) {
+    #endif
+
+    thread(glide, n);
+
+    #ifdef USE_MPI
+    }
+    #endif
     
+    }
+
+    #ifdef USE_MPI
+    MPI_Finalize();
+    #endif
+
     return 0;
 }
